f4fbaa6bc9c0c0d1d08967e56d25096ee7dab0d1
Bin <zzbsdh000@outlook.com>
2023-01-17 21:54:13 -0330

    Add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
 import { useState } from "react";
 import "./App.css";
+import Box from "./Box";
 
 function App() {
-  return <div className="App">Hello Hello!</div>;
+  return (
+    <div className="app">
+      <div className="board">
+        <h1 className="title">Tenzies</h1>
+        <p className="info">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
+        <div className="boxes">
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+          <Box />
+        </div>
+        <button className="btn">Roll</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
