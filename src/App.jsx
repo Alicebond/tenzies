335a823f732976ee93f76ae341de97c41dfc7a97
Bin <zzbsdh000@outlook.com>
2023-02-18 17:08:27 -0330

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,77 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
 import "./App.css";
 import Box from "./Box";
 
 function App() {
+  const [arrNum, setArrNum] = useState(allNewDice());
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    const value = arrNum[0].value;
+    const isWon = arrNum.every((i) => i.isHeld && i.value === value);
+    if (isWon) setTenzies(true);
+  }, [arrNum]);
+
+  function getRandomNum() {
+    return Math.ceil(Math.random() * 6);
+  }
+
+  function allNewDice() {
+    let newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        value: getRandomNum(),
+        isHeld: false,
+        id: nanoid(),
+      });
+    }
+
+    return newDice;
+  }
+
+  function rollDice() {
+    tenzies
+      ? setArrNum(allNewDice())
+      : setArrNum((prev) =>
+          prev.map((i) => (i.isHeld ? i : { ...i, value: getRandomNum() }))
+        );
+  }
+
+  function holdDice(id) {
+    setArrNum((prev) =>
+      prev.map((i) => (i.id === id ? { ...i, isHeld: !i.isHeld } : i))
+    );
+  }
+
+  const boxes = arrNum.map((i) => (
+    <Box
+      isHeld={i.isHeld}
+      id={i.id}
+      num={i.value}
+      key={i.nanoid}
+      handleClick={holdDice}
+    />
+  ));
+
   return (
-    <div className="app">
-      <div className="board">
+    <main className="app">
+      <div className="info">
         <h1 className="title">Tenzies</h1>
-        <p className="info">
-          Roll until all dice are the same. Click each die to freeze it at its
-          current value between rolls.
-        </p>
-        <div className="boxes">
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-        </div>
-        <button className="btn">Roll</button>
+        {tenzies ? (
+          <p className="won">You Won 🎉</p>
+        ) : (
+          <p className="info">
+            Roll until all dice are the same. Click each die to freeze it at its
+            current value between rolls.
+          </p>
+        )}
       </div>
-    </div>
+      <div className="boxes">{boxes}</div>
+      <button className="btn" onClick={rollDice}>
+        {tenzies ? "New Game" : "Roll"}
+      </button>
+    </main>
   );
 }
 
--- src/Box.jsx
+++ src/Box.jsx
@@ -1,3 +1,13 @@
-export default function Box() {
-  return <div className="box"></div>;
+export default function Box(props) {
+  let styles = { backgroundColor: props.isHeld ? "#59E391" : "#fff" };
+
+  return (
+    <div
+      className="box"
+      onClick={() => props.handleClick(props.id)}
+      style={styles}
+    >
+      {props.num}
+    </div>
+  );
 }
